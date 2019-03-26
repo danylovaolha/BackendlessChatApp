08f0ada8615abc5be78f47709385ca5a29bd961a
Olha Danylova <olga@themidnightcoders.com>
2019-03-26 13:09:23 +0200

    Initial Commit

---
+++ BackendlessChatApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BackendlessChatApp
+//
+//  Created by Olga Danylova on /26/319.
+//  Copyright © 2019 Olga Danylova. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
